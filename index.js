1674ec9835eb081e3209f8479f477a18ff72b5a8
adhirajs <adhiraj.sood@loylty.com>
2018-04-18 11:45:41 +0530

    test

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
 var express = require('express');
-//var mysql      = require('mysql');
 var bodyParser = require('body-parser') 
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
+app.set('port', (process.env.PORT || 3000));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+app.post('/register',function(request,response){
 
+	if (request.body.email!=null && request.body.password!=null && request.body.phone!=null) {
 
-//to get all products
-app.get('/product/all',function(request,response){
-
-	response.send('hey')
+		var returnObj = {
+				    		status: "1",
+				    		message: "Registered succesfully.",
+				    		body: {
+				    			email: request.body.email,
+				    			phone: request.body.phone
+				    	}
+				    }
+		response.status(200).send(returnObj);
+	}else{
+		var returnObj = {
+				    		status: "0",
+				    		message: "Registeration failed.",
+				    		body: {}
+				    	};
+		response.status(403).send(returnObj);
+	}
 });
 
 
