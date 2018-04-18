5d3bc7d2751d7c81067b7541332e3a3aa16a0447
adhirajs <adhiraj.sood@loylty.com>
2018-04-18 12:10:47 +0530

    test

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+app.get('/test',function(request,response){
+		response.send("working")
+});
+
 app.post('/register',function(request,response){
 
 	if (request.body.email!=null && request.body.password!=null && request.body.phone!=null) {
